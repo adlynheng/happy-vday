17e6b4ae3a99a6edb1b25e0d2f4e2552e18d3f37
Adlyn Heng <adlynheng@Adlyns-MacBook-Pro.local>
2024-02-14 02:59:03 +0800

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { useState } from "react"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div class="typewriter">
+          <h3>Hello Sean</h3>
+          <h3>This vday gift is from your GF</h3>
+          <h3>Me, Adlyn 😗</h3>
+          <button> 
+            <div class="left"></div>
+            <div class="right"></div>
+            <div class="content"><span>Click to continue</span></div>
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
